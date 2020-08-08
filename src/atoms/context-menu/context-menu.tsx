a6c906fd789380589ba5cf5a0e2e3d578db043a6
Dedok3d <sersh_96@mail.ru>
2020-08-08 19:41:30 +0700

    feat(ContextMenu): add animation to show and hide dropContent

--- src/atoms/context-menu/context-menu.tsx
+++ src/atoms/context-menu/context-menu.tsx
@@ -2,16 +2,50 @@ import React, { useState } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import DownArrow from '../svg/down-arrow';
 
-const hoverAnim = {
-    '0%': {
-        backgroundColor: 'rgb(255,255,255)',
+const [hoverAnim, fadeIn, fadeOut] = [
+    {
+        '0%': {
+            backgroundColor: 'rgb(255,255,255)',
+        },
+        '100%': {
+            backgroundColor: 'rgb(220,220,220)',
+        }
     },
-    '100%': {
-        backgroundColor: 'rgb(220,220,220)',
-    }
-};
+    {
+        '0%': {
+            opacity: 0,
+            top: '75px',
+        },
+        '100%': {
+            opacity: 1,
+            top: '55px',
+        }
+    },
+    {
+        '0%': {
+            opacity: 1,
+            top: '55px',
+        },
+        '100%': {
+            opacity: 0,
+            top: '75px',
+        }
+    },
+];
 
 const styles = StyleSheet.create({
+    animFadeIn: {
+        animationName: [fadeIn],
+        animationDuration: '.5s',
+        animationIterationCount: 1,
+        animationFillMode: 'forwards'
+    },
+    animFadeOut: {
+        animationName: [fadeOut],
+        animationDuration: '.5s',
+        animationIterationCount: 1,
+        animationFillMode: 'forwards'
+    },
     menu: {
         display: 'flex',
         cursor: 'pointer',
@@ -24,7 +58,6 @@ const styles = StyleSheet.create({
         color: 'rgb(0,0,0)',
         width: '150px',
         position: 'absolute',
-        top: '55px',
         overflow: 'hidden',
     },
     ul: {
@@ -50,6 +83,8 @@ interface Props {
 
 function ContextMenu({ options }: Props) {
     const [showSropContent, setShow] = useState(false);
+
+
     return (
         <div className={css(styles.menu)}>
             <div onClick={() => setShow(!showSropContent)}>
@@ -57,7 +92,7 @@ function ContextMenu({ options }: Props) {
                 <DownArrow />
             </div>
             {
-                showSropContent && <div className={css(styles.dropContent)}>
+                <div className={css(styles.dropContent, showSropContent ? styles.animFadeIn : styles.animFadeOut)}>
                     <ul className={css(styles.ul)}>
                         {
                             options.map(option => <li className={css(styles.li)} key={option}>{option}</li>)
