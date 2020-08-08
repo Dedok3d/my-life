9d94b3a41da6bfbb08d81b860b8108834f72b25b
Dedok3d <sersh_96@mail.ru>
2020-08-08 18:17:25 +0700

    feat(ContextMenu): init it

---
+++ src/atoms/context-menu/context-menu.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+    menu: {
+    },
+});
+
+interface Props {
+    options: string[];
+}
+
+function ContextMenu({ }: Props) {
+    return (
+        <div className={css(styles.menu)}>
+        </div>
+    );
+}
+
+export default ContextMenu;
