322fc0f84e4d5f8e6a5fc7098be08d40cce64717
Dedok3d <sersh_96@mail.ru>
2020-08-18 19:15:53 +0700

    feat(OptionsPanel): add FamousOption to render

--- src/components/organisms/options-panel/options-panel.tsx
+++ src/components/organisms/options-panel/options-panel.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
+import { connect, ConnectedProps } from 'react-redux';
 
 import IternalOption from '../../atoms/iternal-option';
 import PersonalOption from '../../atoms/personal-option';
+import FamousOption from '../../atoms/famous-option';
+
 import OptionCard from '../../molecules/option-card';
 import { RootState } from '../../../store/reducers';
-import { connect, ConnectedProps } from 'react-redux';
 import { OptionName } from '../../../models';
 
 
@@ -29,20 +31,27 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 interface Props extends PropsFromRedux { }
 
 function OptionsPanel({ options }: Props) {
-    const [person, stageOfLife] = [
+    const [person, stageOfLife, famousDeaths] = [
         options.find(option => option.name === OptionName.Person),
-        options.find(option => option.name === OptionName.StageOfLife)
+        options.find(option => option.name === OptionName.StageOfLife),
+        options.find(option => option.name === OptionName.FamousDeaths)
     ];
     return (
         <div className={css(styles.panel)}>
             {
-                person.checked && <OptionCard>
+                person && person.checked && <OptionCard>
                     <PersonalOption />
                 </OptionCard>
             }
 
             {
-                stageOfLife.checked && <OptionCard>
+                famousDeaths && famousDeaths.checked && <OptionCard>
+                    <FamousOption />
+                </OptionCard>
+            }
+
+            {
+                famousDeaths && stageOfLife.checked && <OptionCard>
                     <IternalOption />
                 </OptionCard>
             }
