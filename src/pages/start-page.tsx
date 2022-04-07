5d43a02c6da48c7dd4df2f2849b3af303262df13
Dedok3d <sersh_96@mail.ru>
2022-04-07 13:57:47 +0700

    feat(components): удалить лишние пропсы
    Полностью!
    МЕТА ДАННЫЕ: LOL-1

--- src/pages/start-page.tsx
+++ src/pages/start-page.tsx
@@ -14,9 +14,7 @@ const styles = StyleSheet.create({
     },
 });
 
-interface Props { }
-
-function StartPage({ }: Props) {
+function StartPage() {
     return (
         <div className={css(styles.main)}>
             <HeaderPanel />
